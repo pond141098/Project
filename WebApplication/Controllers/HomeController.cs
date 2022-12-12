99108571ddb54d3ea115f503e91848aa193b296c
pasathon phrompayak <pasathon.btg141098@outlook.com>
2022-12-12 13:33:10 +0700

    สร้างTemplateฝั่งอาจารย์

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace WebApplication.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public IActionResult Privacy()
